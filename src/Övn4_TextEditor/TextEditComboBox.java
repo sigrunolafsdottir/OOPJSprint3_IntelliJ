f13a6a31670f538168678949b52b90d72badc2ad
sigrunolafsdottir <solafsdottir@gmail.com>
2023-10-27 11:49:25 +0200

    updated JCombobox-example

--- src/Övn4_TextEditor/TextEditComboBox.java
+++ src/Övn4_TextEditor/TextEditComboBox.java
@@ -14,37 +14,39 @@ import javax.swing.*;
    private final String fileNameCachePath = "src\\Övn4_TextEditor\\fileNameCache.txt";
    private JPanel p = new JPanel(); 
    Vector<String> fileCache = new Vector<>();
+
+   String[] fileCacheArr = new String[5];
    private JComboBox<String> namn; 
    private JButton    öppna = new JButton("Öppna"); 
    private JButton    spara = new JButton("Spara");
    private JButton    skriv = new JButton("Skriv ut");
    private JButton    sluta = new JButton("Avsluta");
-   private JTextArea  area  = new JTextArea(10,60); 
+   private JTextArea  area  = new JTextArea(10,40);
    private JScrollPane sp   = new JScrollPane(area,
                        JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
                        JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 
-   public TextEditComboBox() { 
+   public TextEditComboBox() {
      laddaFilnamn();
-     namn = new JComboBox(fileCache);
+      namn = new JComboBox(fileCacheArr);
      namn.setSelectedIndex(-1);
-     area.setFont(new Font("Monospaced", Font.PLAIN, 12)); 
-     p.setLayout(new GridLayout(1,6)); 
-     p.add(new JLabel("Filnamn: ", JLabel.RIGHT));  
-     p.add(namn); p.add(öppna); p.add(spara); 
-     p.add(skriv); p.add(sluta); 
+     area.setFont(new Font("Monospaced", Font.PLAIN, 12));
+     p.setLayout(new GridLayout(1,6));
+     p.add(new JLabel("Filnamn: ", JLabel.RIGHT));
+     p.add(namn); p.add(öppna); p.add(spara);
+     p.add(skriv); p.add(sluta);
      namn.setEditable(true);
-     namn.addActionListener(this);   
-     öppna.addActionListener(this);  
-     spara.addActionListener(this); 
+     namn.addActionListener(this);
+     öppna.addActionListener(this);
+     spara.addActionListener(this);
      skriv.addActionListener(this);
      sluta.addActionListener(this);
-     add(p,  BorderLayout.NORTH); 
-     add(sp, BorderLayout.CENTER); 
-     pack(); 
-     setVisible(true); 
-     setDefaultCloseOperation(EXIT_ON_CLOSE);  
-   } 
+     add(p,  BorderLayout.NORTH);
+     add(sp, BorderLayout.CENTER);
+     pack();
+     setVisible(true);
+     setDefaultCloseOperation(EXIT_ON_CLOSE);
+   }
 
    
    public void actionPerformed(ActionEvent e) { 
@@ -78,12 +80,13 @@ import javax.swing.*;
    
    private void laddaFilnamn() { 
      try (FileReader r = new FileReader(fileNameCachePath); 
-        Scanner sc = new Scanner(r);){ 
-         
-        while(sc.hasNextLine()){
-            fileCache.add(sc.nextLine().trim());
+        Scanner sc = new Scanner(r);){
+         int i = 0;
+         while(sc.hasNextLine() && i < 5){
+            //fileCache.add(sc.nextLine().trim());
+            fileCacheArr[i] = sc.nextLine().trim();
+            i++;
         }
-        r.close();
      } 
      catch (IOException e) {
         e.printStackTrace();
